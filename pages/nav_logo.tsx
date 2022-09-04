08d07e522023342096f6015d242b0400b1494cd0
Maxime <sowlution.pro@gmail.com>
2022-09-04 04:38:39 +0200

    petite modif

--- pages/nav_logo.tsx
+++ pages/nav_logo.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import * as React from 'react'
 import styles from '../styles/Home.module.css';
 const NavbarScroller = () => {
   return (
     <div>
         <img src="../logo.png" className={styles.card_img_ld} ></img>
-        <a className={styles.logo_blase} href="/">ZK-Rent</a>
+        <Link className={styles.logo_blase} href="/">ZK-Rent</Link>
     </div>
   )
 }
