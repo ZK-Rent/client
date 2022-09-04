be18adb03466f8914742eaa7a7918d588407a19e
Bertrand Juglas <b@juglas.name>
2022-09-04 07:18:48 +0200

    fix: use capital letter for React component

--- pages/landlords/makeOffer.tsx
+++ pages/landlords/makeOffer.tsx
@@ -1,42 +1,38 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../../styles/Home.module.css';
-import Link from 'next/link';
-import NavbarScroller from '../nav_logo';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
+import NavbarScroller from "../nav_logo";
 
-import * as React from 'react';
+import * as React from "react";
 
-import { usePrepareContractWrite, useContractWrite } from 'wagmi';
+import { usePrepareContractWrite, useContractWrite } from "wagmi";
 
 import offerFactoryABI from "../../artifacts/contracts/OfferFactory.sol/OfferFactory.json";
 
-const makeOffer: NextPage = () => {
-
-  const [description, setDescription] = React.useState('');
-  const [incomeRequirement, setIncomeRequirement] = React.useState('');
-  const [depositRequirement, setDepositRequirement] = React.useState('');
+const MakeOffer: NextPage = () => {
+  const [description, setDescription] = React.useState("");
+  const [incomeRequirement, setIncomeRequirement] = React.useState("");
+  const [depositRequirement, setDepositRequirement] = React.useState("");
 
   const { config } = usePrepareContractWrite({
-    addressOrName: '0x8fd720Fb3F96859A90135c5efc74901B3cA18443',
-    contractInterface: offerFactoryABI,
-    functionName: 'newOffer',
-  })
+    addressOrName: "0x8fd720Fb3F96859A90135c5efc74901B3cA18443",
+    contractInterface: offerFactoryABI.abi,
+    functionName: "newOffer",
+  });
 
   const { write } = useContractWrite({
     ...config,
-    functionName: 'newOffer',
-    args: [description,incomeRequirement,depositRequirement],
-  })
+    functionName: "newOffer",
+    args: [description, incomeRequirement, depositRequirement],
+  });
 
   return (
     <div className={styles.container}>
       <Head>
         <title>ZK Rental</title>
-        <meta
-          name="description"
-          content="ZK Rent plateform"
-        />
+        <meta name="description" content="ZK Rent plateform" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavbarScroller></NavbarScroller>
@@ -44,27 +40,43 @@ const makeOffer: NextPage = () => {
       <main className={styles.main}>
         <ConnectButton />
 
-        <h1 className={styles.title}>
-          Make an offer !
-        </h1>
-        <p>Submit an offer and allow your future tenants to submit their application in a secure manner.
+        <h1 className={styles.title}>Make an offer !</h1>
+        <p>
+          Submit an offer and allow your future tenants to submit their
+          application in a secure manner.
         </p>
         <form>
           <label className={styles.my_input}>
             Description :
-            <input type="text" name="description" onchange={(e) => setDescription(e.target.value)} />
+            <input
+              type="text"
+              name="description"
+              onChange={(e) => setDescription(e.target.value)}
+            />
           </label>
           <label className={styles.my_input}>
             Income requirement :
-            <input type="text" name="income" onchange={(e) => setIncomeRequirement(e.target.value)}/>
+            <input
+              type="text"
+              name="income"
+              onChange={(e) => setIncomeRequirement(e.target.value)}
+            />
           </label>
           <label className={styles.my_input}>
             Deposit amount :
-            <input type="text" name="deposit_amount" onchange={(e) => setDepositRequirement(e.target.value)}/>
+            <input
+              type="text"
+              name="deposit_amount"
+              onChange={(e) => setDepositRequirement(e.target.value)}
+            />
           </label>
-          <input disabled={!write} onClick={() => write?.()} type="submit" value="Create offer" />
+          <input
+            disabled={!write}
+            onClick={() => write?.()}
+            type="submit"
+            value="Create offer"
+          />
         </form>
-       
       </main>
 
       <footer className={styles.footer}>
@@ -76,4 +88,4 @@ const makeOffer: NextPage = () => {
   );
 };
 
-export default makeOffer;
+export default MakeOffer;
